cb5965bfb8572fa28c95122b99ba339a6b39b5d4
Hedi SELLAMI <hedi@sellami.dev>
2025-10-04 15:50:31 +0200

    Fix indentation again

--- scripts/battle_area.gd
+++ scripts/battle_area.gd
@@ -5,17 +5,17 @@ var boundaries: StaticBody2D
 var enter_zone: Area2D
 
 func _ready():
-    boundaries = $Boundaries
-    enter_zone = $EnterZone
+	boundaries = $Boundaries
+	enter_zone = $EnterZone
 
-    enter_zone.body_entered.connect(on_area_body_entered)
+	enter_zone.body_entered.connect(on_area_body_entered)
 
 func on_area_body_entered(body):
-    if body is Player:
-        trigger_battle()
+	if body is Player:
+		trigger_battle()
 
 func trigger_battle():
-    boundaries.process_mode = Node.PROCESS_MODE_ALWAYS
-    enter_zone.set_deferred("process_mode", Node.PROCESS_MODE_DISABLED)
-    Global.start_battle()
-    Global.camera.reparent_smoothly(self)
+	boundaries.process_mode = Node.PROCESS_MODE_ALWAYS
+	enter_zone.set_deferred("process_mode", Node.PROCESS_MODE_DISABLED)
+	Global.start_battle()
+	Global.camera.reparent_smoothly(self)
--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -3,12 +3,12 @@ extends Camera2D
 class_name Camera2DPlus
 
 func _ready():
-    Global.camera = self
+	Global.camera = self
 
 func reparent_smoothly(new_parent: Node):
-    var camera_position = global_position
-    reparent(new_parent, true)
-    global_position = camera_position
+	var camera_position = global_position
+	reparent(new_parent, true)
+	global_position = camera_position
 
-    var camera_tween = create_tween()
-    camera_tween.tween_property(self, "position", Vector2(0, 0), 0.5)
+	var camera_tween = create_tween()
+	camera_tween.tween_property(self, "position", Vector2(0, 0), 0.5)
