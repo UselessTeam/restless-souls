68168ef78507df4cab77aeaac461ecac68cd57a2
Hedi SELLAMI <hedi@sellami.dev>
2025-10-04 20:53:33 +0200

    Change movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,20 +7,28 @@ class_name Player
 
 @onready var animated_sprite = $AnimatedSprite2D
 
+var can_move := true
+
 func _ready():
     Global.player = self
     animated_sprite.animation_finished.connect(_on_attack_animation_finished)
 
 func _process(_delta):
-    var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+    if !can_move:
+        return
+    var direction = Input.get_vector("left", "right", "up", "down")
     velocity = direction * speed
     move_and_slide()
 
 
 func _unhandled_input(event):
+    if !can_move:
+        return
     if event.is_action_pressed("ui_accept"):
         animated_sprite.play("attack")
+        can_move = false
 
 func _on_attack_animation_finished():
     if (animated_sprite.animation == "attack"):
         animated_sprite.play("idle")
+        can_move = true
