a636118e5a46d549a9df54f617aea023372dea25
Swynfel <git@swynfel.eu>
2025-10-04 16:32:23 +0200

    Add global signals and hooks

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,10 +1,27 @@
 extends Node
 
+enum GamePhase {LOADING, ROAM, BATTLE}
+
 var player: Player = null
 var camera: Camera2DPlus = null
+var phase: GamePhase = GamePhase.LOADING
+
+signal battle_phase_start()
+signal battle_phase_end()
+signal game_phase_change(previous: GamePhase, next: GamePhase)
 
 func start_battle():
-    print("Battle started!")
+    battle_phase_start.emit()
+    game_phase_change.emit(phase, GamePhase.BATTLE)
+    phase = GamePhase.BATTLE
 
 func end_battle():
-    print("Battle ended!")
+    battle_phase_end.emit()
+    game_phase_change.emit(phase, GamePhase.ROAM)
+    phase = GamePhase.ROAM
+
+func is_battling() -> bool:
+    return phase == GamePhase.BATTLE
+
+func is_roaming() -> bool:
+    return phase == GamePhase.ROAM
