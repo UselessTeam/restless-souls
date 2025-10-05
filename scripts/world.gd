b5a2e14ad565159726b41de7d8561aefbe0fa9e7
maria <merybdldz@gmail.com>
2025-10-05 17:29:02 +0200

    Add world prototype and buddas

--- scripts/world.gd
+++ scripts/world.gd
@@ -3,5 +3,5 @@ extends Node2D
 class_name World
 
 func _enter_tree():
-    Global.world = self
-    Global.phase = Global.GamePhase.ROAM
+	Global.world = self
+	Global.phase = Global.GamePhase.ROAM
