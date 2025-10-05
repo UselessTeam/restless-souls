4fabf5e652a1d8096790b8f54249531f1c8939be
Hedi SELLAMI <hedi@sellami.dev>
2025-10-05 19:36:20 +0200

    Add simple dialogs

---
+++ scripts/dialog_box.gd
@@ -0,0 +1,59 @@
+extends ColorRect
+class_name DialogBox
+
+@onready var texture_rect = $TextureRect
+@onready var label = $Label
+
+@export var player: Texture2D
+@export var burned: Texture2D
+@export var drowned: Texture2D
+@export var electrocuted: Texture2D
+@export var knife: Texture2D
+@export var sad: Texture2D
+
+const MIN_SHOW_TIME = 1
+const FADE_IN_OUT_TIME = 0.1
+
+var text_showing
+signal text_finished()
+signal skip_text()
+
+func _ready():
+    modulate.a = 0
+    Global.dialog_box = self
+    await get_tree().create_timer(MIN_SHOW_TIME).timeout
+    display_text("player", "I heard some restless spirits are wreaking havoc around this town.\nTime to collect some souls.")
+
+func display_text(texture_name, text, leave_on = false):
+    text_showing = true
+    texture_rect.texture = get_texture(texture_name)
+    label.text = text
+    print("displaying")
+    await create_tween().tween_property(self, "modulate:a", 1, FADE_IN_OUT_TIME).finished
+    await get_tree().create_timer(MIN_SHOW_TIME).timeout
+    await skip_text
+    if !leave_on:
+        await create_tween().tween_property(self, "modulate:a", 0, FADE_IN_OUT_TIME).finished
+    text_finished.emit()
+
+func get_texture(texture_name: String) -> Texture2D:
+    match texture_name:
+        "player":
+            return player
+        "burned":
+            return burned
+        "drowned":
+            return drowned
+        "electrocuted":
+            return electrocuted
+        "knife":
+            return knife
+        "sad":
+            return sad
+        _:
+            push_warning("Unknown texture name: %s" % texture_name)
+            return null
+
+func _input(event: InputEvent) -> void:
+    if event.is_action_pressed("action_use"):
+        skip_text.emit()
