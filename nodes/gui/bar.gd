5bff668082c5d670ee38fc3658c4fe1f9996c6f2
Swynfel <git@swynfel.eu>
2025-10-05 12:29:32 +0200

    Add knots to energy bar

--- nodes/gui/bar.gd
+++ nodes/gui/bar.gd
@@ -10,6 +10,7 @@ class_name Bar
 
 @export var padding: float = 0.0
 @export var y_margin: float = 0.0
+@export var x_margin: float = 0.0
 
 func _notification(what):
     if what == NOTIFICATION_SORT_CHILDREN:
@@ -17,7 +18,7 @@ func _notification(what):
 
 func _sort_children() -> void:
     var rect: Rect2 = get_rect()
-    var width = (rect.size.x - 2 * padding) * fraction + 2 * padding
+    var width = (rect.size.x - 2 * (x_margin + padding)) * fraction + 2 * padding
     for child in get_children():
         if is_instance_valid(child) and child is Control:
-            fit_child_in_rect(child, Rect2(Vector2(0, y_margin), Vector2(width, rect.size.y - 2 * y_margin)))
+            fit_child_in_rect(child, Rect2(Vector2(x_margin, y_margin), Vector2(width, rect.size.y - 2 * y_margin)))
--- scripts/gui/energy-bar.gd
+++ scripts/gui/energy-bar.gd
@@ -10,25 +10,28 @@ const MIN_ENERGY: float = -0.1
 @onready var base_energy_bar: Bar = $Base
 @onready var spell_energy_bar: Bar = $Spell
 @onready var walk_energy_bar: Bar = $Walk
+@onready var knots: Node = $Knots/List
 
 var player_last_position: Vector2 = Vector2.ZERO
 
-var max_energy: float = MAX_ENERGY:
+var max_energy: float = 0.9:
     set(value):
-        max_energy = max(value, 1.0)
-        update_bars()
+        if max_energy != value:
+            max_energy = max(value, 1.0)
+            update_knots_count()
+            update_bars_and_knots()
 var step_energy: float = max_energy:
     set(value):
         step_energy = value
-        update_bars()
+        update_bars_and_knots()
 var walk_energy_drain: float = 0.0:
     set(value):
         walk_energy_drain = value
-        update_bars()
+        update_bars_and_knots()
 var reserved_energy_for_spell: float = 0.0:
     set(value):
         reserved_energy_for_spell = value
-        update_bars()
+        update_bars_and_knots()
 var energy_before_spell: float:
     get:
         return step_energy - walk_energy_drain
@@ -36,10 +39,39 @@ var next_energy: float:
     get:
         return step_energy - walk_energy_drain - reserved_energy_for_spell
 
-func update_bars():
+func update_knots_count():
+    var target_knot_count = floori(max_energy) + 1
+    while knots.get_child_count() < target_knot_count:
+        var new_knot = knots.get_child(0).duplicate()
+        new_knot.name = "Knot-%d" % (knots.get_child_count() - 1)
+        knots.add_child(new_knot)
+    print(target_knot_count, " ", knots.get_child_count())
+    for delete_index in range(target_knot_count, knots.get_child_count()):
+        knots.get_child(delete_index).queue_free()
+    
+    for i in range(target_knot_count):
+        var knot: Control = knots.get_child(i)
+        knot.anchor_left = i / max_energy
+        knot.anchor_right = i / max_energy
+
+func update_bars_and_knots():
     base_energy_bar.fraction = next_energy / max_energy
     spell_energy_bar.fraction = energy_before_spell / max_energy
     walk_energy_bar.fraction = step_energy / max_energy
+
+    for i in range(knots.get_child_count()):
+        var knot: Control = knots.get_child(i)
+        if i > step_energy:
+            knot.hide()
+            continue
+        knot.show()
+        if i < next_energy:
+            knot.modulate = base_energy_bar.modulate
+        elif i < energy_before_spell:
+            knot.modulate = spell_energy_bar.modulate
+        else:
+            knot.modulate = walk_energy_bar.modulate
+
 
 func start_battle():
     max_energy = MAX_ENERGY
