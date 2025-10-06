b63d7cff4d6bff6cfbf87de392484c6b7368242c
Hedi SELLAMI <hedi@sellami.dev>
2025-10-06 22:07:06 +0200

    Fix first battle

--- scripts/death_tuto.gd
+++ scripts/death_tuto.gd
@@ -5,4 +5,5 @@ class_name DeathTuto
 func act_turn():
     if Global.player.global_position.distance_to(global_position) > 400:
         return
+    Global.battle.battle_area.monsters.clear()
     take_damage(1000)
